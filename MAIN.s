8c66ecd3ded85d741d7e6f8497c94904130bb97c
koney@koney.org <koney@koney.org>
2020-12-08 23:18:35 +0000

    added SPRITE logo

--- MAIN.s
+++ MAIN.s
@@ -19,24 +19,56 @@ MODSTART_POS=0		; start music at position # !! MUST BE EVEN FOR 16BIT
 ;********** Demo **********	; Demo-specific non-startup code below.
 Demo:	;a4=VBR, a6=Custom Registers Base addr
 	;*--- init ---*
-	move.l	#VBint,$6c(a4)
-	move.w	#%1110000000100000,INTENA
+	MOVE.L	#VBint,$6C(A4)
+	MOVE.W	#%1110000000100000,INTENA
 	;** SOMETHING INSIDE HERE IS NEEDED TO MAKE MOD PLAY! **
 	;move.w	#%1110000000000000,INTENA	; Master and lev6	; NO COPPER-IRQ!
-	move.w	#%1000011111000000,DMACON
+	MOVE.W	#%1000011111100000,DMACON
 
 	;*--- clear screens ---*
-	lea	Screen1,a1
+	lea	SCREEN1,a1
 	bsr.w	ClearScreen
-	lea	Screen2,a1
+	lea	SCREEN2,a1
 	bsr.w	ClearScreen
 	bsr	WaitBlitter
 	;*--- start copper ---*
-	lea	Screen1,a0
+	lea	SCREEN1,a0
 	moveq	#bpl,d0
 	lea	BplPtrs+2,a1
 	moveq	#bpls-1,d1
 	bsr.w	PokePtrs
+
+	; #### Point LOGO sprites
+	LEA	SpritePointers,A1	; Puntatori in copperlist
+	MOVE.L	#SPRT_K,D0	; indirizzo dello sprite in d0
+	MOVE.W	D0,6(A1)
+	SWAP	D0
+	MOVE.W	D0,2(A1)
+
+	ADDQ.W	#8,A1
+	MOVE.L	#SPRT_O,D0	; indirizzo dello sprite in d0
+	MOVE.W	D0,6(A1)
+	SWAP	D0
+	MOVE.W	D0,2(A1)
+
+	ADDQ.W	#8,A1
+	MOVE.L	#SPRT_N,D0	; indirizzo dello sprite in d0
+	MOVE.W	D0,6(A1)
+	SWAP	D0
+	MOVE.W	D0,2(A1)
+
+	ADDQ.W	#8,A1
+	MOVE.L	#SPRT_Y,D0	; indirizzo dello sprite in d0
+	MOVE.W	D0,6(A1)
+	SWAP	D0
+	MOVE.W	D0,2(A1)
+
+	ADDQ.W	#8,A1
+	MOVE.L	#SPRT_E,D0	; indirizzo dello sprite in d0
+	MOVE.W	D0,6(A1)
+	SWAP	D0
+	MOVE.W	D0,2(A1)
+	; #### Point LOGO sprites
 
 	;---  Call P61_Init  ---
 	MOVEM.L	D0-A6,-(SP)
@@ -48,7 +80,9 @@ Demo:	;a4=VBR, a6=Custom Registers Base addr
 	MOVEM.L (SP)+,D0-A6
 	;---  Call P61_Init  ---
 
-	MOVE.L	#Copper,$80(a6)
+	MOVE.L	#Copper,$80(A6)
+
+	MOVE.W	#$8000,VPOSW	; RESETS LOF (from EAB)
 
 ;********************  main loop  ********************
 MainLoop:
@@ -70,31 +104,23 @@ MainLoop:
 	bsr	WaitBlitter
 
 	; ** CODE FOR HI-RES ** FROM Lezione11l6.S ********************
-	;MOVE.L	#$1FF00,D1	; bit per la selezione tramite AND
-	;MOVE.L	#$01000,D2	; linea da aspettare = $010
-	;Waity1:
-	;MOVE.L	VPOSR,D0		; VPOSR e VHPOSR - $dff004/$dff006
-	;AND.L	D1,D0		; Seleziona solo i bit della pos. verticale
-	;CMP.L	#0,D0		; aspetta la linea $010
-	;BNE.S	Waity1
-
-	MOVE.L	KONEYBG,D3	; Indirizzo bitplane
-	;BTST.B	#15-8,VPOSR	; VPOSR LOF bit?
-	MOVE.W	IsLineEven,D7	; GET ODD/EVEN BIT
-	NOT.W	D7		; CHANGE IT
-	MOVE.W	D7,IsLineEven	; PUT IT BACK
-	CMPI.W	#0,D7		; IF 0 SKIP A LINE
-	BEQ.S	.skipLine		; Se si, tocca alle linee dispari
-	ADD.L	#bpl,D3		; Oppure aggiungi la lunghezza di una linea,
+	MOVE.L	KONEY,D3		; Indirizzo bitplane
+	MOVE.W	VPOSR,D7
+	BTST	#15,D7
+	BNE.S	.skipLine		; Se si, tocca alle linee dispari
+	ADD.L	#bpl,D3		; Oppure aggiungi la lunghezza di una linea
 	.skipLine:
 
 	; ** CODE FOR HI-RES **************************************
 	MOVE.L	D3,DrawBuffer
-
+	;CLR.W	$100		; DEBUG | w 0 100 2
+	;CLR.W	$100		; THIS IS NEEDED FOR HI-RES... LOL!!
 	; do stuff here :)
 
-	;CLR.W	$100		; DEBUG | w 0 100 2
-	CLR.W	$100		; THIS IS NEEDED FOR HI-RES... LOL!!
+	;move.w	$000,$DFF18E	; metti VHPOSR in COLOR00 (lampeggio!!)
+	;bsr.w	__DUMMY		; Stampa le linee di testo sullo schermo
+	;move.w	$222,$DFF18E	; metti VHPOSR in COLOR00 (lampeggio!!)
+	;bsr.w	__DUMMY		; Stampa le linee di testo sullo schermo
 
 	;*--- main loop end ---*
 	BTST	#6,$BFE001
@@ -145,12 +171,31 @@ VBint:				; Blank template VERTB interrupt
 
 	RTS
 
+__DUMMY:
+	LEA	DUMMY,A3		; Indirizzo del bitplane destinazione in a3
+	LEA	DUMMY,A2
+	CLR	D6
+	MOVE.B	#40,D6		; RESET D6
+	;ADD.W	#40*115,A3	; POSITIONING
+
+	.loop:			; LOOP KE CICLA LA BITMAP
+	;ADD.W	#15,A3
+	MOVE.L	(A2),(A3)	
+	;ADD.W	#25,A3
+	DBRA	D6,.loop
+
+	RTS
+
 ;********** Fastmem Data **********
 IsLineEven:	DC.W 0
-KONEYBG:		DC.L BG1		; INIT BG
-DrawBuffer:	DC.L SCREEN2	; pointers to buffers to be swapped
-ViewBuffer:	DC.L SCREEN1
-
+KONEY:		DC.L BG1		; INIT BG
+DrawBuffer:	DC.L SCREEN1	; pointers to buffers to be swapped
+ViewBuffer:	DC.L SCREEN2
+SPR_0_POS:	DC.B $7C		; K
+SPR_1_POS:	DC.B $84		; O
+SPR_2_POS:	DC.B $8C		; N
+SPR_3_POS:	DC.B $94		; E
+SPR_4_POS:	DC.B $9C		; Y
 ;**************************************************************
 	SECTION "ChipData",DATA_C	;declared data that must be in chipmem
 ;**************************************************************
@@ -158,6 +203,10 @@ ViewBuffer:	DC.L SCREEN1
 BG1:	INCBIN	"klogo_hd.raw"
 
 MODULE:	INCBIN	"take_em_in.P61"	; code $9100
+
+SPRITES:	INCLUDE	"sprite_KONEY.s"
+
+DUMMY:	DC.L $F0F0F0	
 
 Copper:
 	DC.W $1FC,0	;Slow fetch mode, remove if AGA demo.
@@ -175,10 +224,12 @@ Copper:
 	;DC.W $100,bpls*$1000+$200	;enable bitplanes
 	DC.W $100,%1011001000000100	; BPLCON0 1011 0010 0000 0100
 
-Palette:	DC.W $0180,$0FFF,$0182,$0CCC,$0184,$0999,$0186,$0666
-	DC.W $0188,$0444,$018A,$0333,$018C,$0222,$018E,$0000
+	Palette:	
+	DC.W $0180,$0AAA,$0182,$0FFF,$0184,$0888,$0186,$0666
+	DC.W $0188,$0444,$018A,$0333,$018C,$0222,$018E,$0515
 
-BplPtrs:	DC.W $E0,0
+	BplPtrs:	
+	DC.W $E0,0
 	DC.W $E2,0
 	DC.W $E4,0
 	DC.W $E6,0
@@ -191,7 +242,25 @@ BplPtrs:	DC.W $E0,0
 	DC.W $F4,0
 	DC.W $F6,0
 
+	SpritePointers:
+	DC.W $120,0,$122,0	; 0
+	DC.W $124,0,$126,0	; 1
+	DC.W $128,0,$12A,0	; 2
+	DC.W $12C,0,$12E,0	; 3
+	DC.W $130,0,$132,0	; 4
+	DC.W $134,0,$136,0	; 5
+	DC.W $138,0,$13A,0	; 6
+	DC.W $13C,0,$13E,0	; 7
 
+	DC.W $1A6
+	LOGOCOL1:
+	DC.W $FFF	; COLOR0-1
+	DC.W $1AE
+	LOGOCOL2:
+	DC.W $FFF	; COLOR2-3
+	DC.W $1B6
+	LOGOCOL3:
+	DC.W $FFF	; COLOR4-5
 
 	DC.W $FFFF,$FFFE	;magic value to end copperlist
 _Copper:
@@ -203,4 +272,4 @@ _Copper:
 SCREEN1:		DS.B h*bwid	; Define storage for buffer 1
 SCREEN2:		DS.B h*bwid	; two buffers
 
-	END
+END
---
+++ sprite_KONEY.s
@@ -0,0 +1,87 @@
+SPRT_K:	
+	DC.B	$33	; Posizione verticale di inizio sprite (da $2c a $f2)
+	SPRT_K_POS:
+	DC.B	$44	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+	DC.B	$42	; $50+13=$5d	; posizione verticale di fine sprite
+	DC.B	$00
+	DC.W	$E00E,$E00E,$E00E,$E00E,$E00E,$E00E
+	DC.W	$E070,$E070,$E070,$E070,$E070,$E070
+	DC.W	$FF80,$FF80,$FF80,$FF80,$FF80,$FF80
+	DC.W	$FC70,$FC70,$FC70,$FC70,$FC70,$FC70
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+SPRT_O:	
+	DC.B	$33	; Posizione verticale di inizio sprite (da $2c a $f2)
+	SPRT_O_POS:
+	DC.B	$4D	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+	DC.B	$42	; $50+13=$5d	; posizione verticale di fine sprite
+	DC.B	$00
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	$E00E,$E00E,$E00E,$E00E,$E00E,$E00E
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+SPRT_N:	
+	DC.B	$33	; Posizione verticale di inizio sprite (da $2c a $f2)
+	SPRT_N_POS:
+	DC.B	$56	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+	DC.B	$42	; $50+13=$5d	; posizione verticale di fine sprite
+	DC.B	$00
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	$E00E,$E00E,$E00E,$E00E,$E00E,$E00E
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+
+SPRT_E:	
+	DC.B	$33	; Posizione verticale di inizio sprite (da $2c a $f2)
+	SPRT_E_POS:
+	DC.B	$5F	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+	DC.B	$42	; $50+13=$5d	; posizione verticale di fine sprite
+	DC.B	$00
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	$E000,$E000,$E000,$E000,$E000,$E000
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	$FC00,$FC00,$FC00,$FC00,$FC00,$FC00
+	DC.W	$FFFE,$FFFE,$FFFE,$FFFE,$FFFE,$FFFE
+	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+SPRT_Y:	
+	DC.B	$33	; Posizione verticale di inizio sprite (da $2c a $f2)
+	SPRT_Y_POS:
+	DC.B	$68	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+	DC.B	$42	; $50+13=$5d	; posizione verticale di fine sprite
+	DC.B	$00
+	DC.W	$FC7E,$FC7E,$FC7E,$FC7E,$FC7E,$FC7E
+	DC.W	$1FF0,$1FF0,$1FF0,$1FF0,$1FF0,$1FF0
+	DC.W	$0380,$0380,$0380,$0380,$0380,$0380
+	DC.W	$03F0,$03F0,$03F0,$03F0,$03F0,$03F0
+	DC.W	$03F0,$03F0,$03F0,$03F0,$03F0,$03F0
+	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+
+;SPRT_K_BG:	
+;	DC.B	$A4	; Posizione verticale di inizio sprite (da $2c a $f2)
+;	SPRT_K_BG_POS:
+;	DC.B	$92	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+;	DC.B	$B3	; $50+13=$5d	; posizione verticale di fine sprite
+;	DC.B	$00
+;	DC.W	$E00E,$E00E,$E00E,$E00E,$E00E,$E00E
+;	DC.W	$E070,$E070,$E070,$E070,$E070,$E070
+;	DC.W	$FF80,$FF80,$FF80,$FF80,$FF80,$FF80
+;	DC.W	$FC70,$FC70,$FC70,$FC70,$FC70,$FC70
+;	DC.W	$FC0E,$FC0E,$FC0E,$FC0E,$FC0E,$FC0E
+;	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
+
+;SPRT_Y_BG:	
+;	DC.B	$A4	; Posizione verticale di inizio sprite (da $2c a $f2)
+;	SPRT_Y_BG_POS:
+;	DC.B	$BD	; Posizione orizzontale di inizio sprite (da $40 a $d8)
+;	DC.B	$B3	; $50+13=$5d	; posizione verticale di fine sprite
+;	DC.B	$00
+;	DC.W	$FC7E,$FC7E,$FC7E,$FC7E,$FC7E,$FC7E
+;	DC.W	$1FF0,$1FF0,$1FF0,$1FF0,$1FF0,$1FF0
+;	DC.W	$0380,$0380,$0380,$0380,$0380,$0380
+;	DC.W	$03F0,$03F0,$03F0,$03F0,$03F0,$03F0
+;	DC.W	$03F0,$03F0,$03F0,$03F0,$03F0,$03F0
+;	DC.W	0,0	; 2 word azzerate definiscono la fine dello sprite.
